32821ef9354e4fe89feba3279c7374a4ccd76e46
jaredoong <jaredoong@gmail.com>
2023-10-14 15:57:27 +0800

    Integrated in OTA using Blynk

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,9 @@
 // #include <Adafruit_Sensor.h>
 #include <Arduino.h>
 #include <WiFi.h>
-// #include <ESP_Google_Sheet_Client.h>
 #include <Preferences.h>
 #include <vector>
+#include <Update.h>
 #include "time.h"
 
 using namespace std;
@@ -27,6 +27,16 @@ using namespace std;
 unsigned long long deep_sleep_time = 1000000ULL * 10 * 1 * 1; // Time to sleep in microseconds (10seconds)
 Preferences preferences;
 int mode; // 0 for autoencoder, 1 for classifier
+
+//***************** WiFi/OTA variables *****************
+WiFiClient client;
+
+// Variables to validate response from S3
+long contentLength = 0;
+bool isValidContentType = false;
+
+//***************** Blynk variables *****************
+volatile bool OTAAvailable = false;
 
 // **************** Accelerometer variables ****************
 int acc_counter = 0;
@@ -103,11 +113,70 @@ tflite::MicroInterpreter *model_interpreter;
 TfLiteTensor *model_input;
 TfLiteTensor *model_output;
 
+//***************** Blynk Functions *****************
+BLYNK_CONNECTED()
+{
+	Blynk.syncVirtual(OTA_VPIN);
+}
+
+BLYNK_WRITE(OTA_VPIN) // To read in whether OTA is available from Blynk
+{
+	OTAAvailable = param.asInt();
+}
+
+//***************** OTA Functions *****************
+String getHeaderValue(String header, String headerName);
+void execOTA();
+
 // Set up the ESP32's environment.
 void setup()
 {
 	// Start serial at 115200 baud
 	Serial.begin(115200);
+
+	// Connect to Wifi
+	int numConnection = 0;
+	WiFi.setAutoReconnect(true);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.printf("Connecting to %s\n", WIFI_SSID);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		if (numConnection >= 15)
+		{
+			Serial.println("Failed to connect to Wi-Fi");
+			ESP.restart();
+		}
+		Serial.print(".");
+		numConnection++;
+		delay(300);
+	}
+
+	// Check for OTA first
+	// ********** Blynk setup **********
+	Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 8080);
+	if (Blynk.connected())
+	{
+		Serial.println("Blynk Connected");
+		delay(100);
+		Blynk.syncVirtual(OTA_VPIN);
+	}
+	else
+	{
+		Serial.println("Blynk Not Connected");
+	}
+
+	// ************ Execute OTA Update if available ************
+	if (OTAAvailable)
+	{
+		Serial.println("OTA is available");
+		execOTA();
+	}
+	else
+	{
+		Serial.println("OTA is not available");
+	}
+
+	Blynk.virtualWrite(FIRMWARE_VERSION_VPIN, FIRMWARE_VERSION);
 
 	// Check which mode is supposed to be in
 	preferences.begin("mode", false);	  // Open preferences with write access
@@ -159,15 +228,12 @@ void setup()
 	model_output = model_interpreter->output(0);
 	Serial.println("Input and output nodes loaded!");
 	Serial.println("Starting inferences");
-
-	// // Load mode back to 0 to ensure autoencoder is loaded next time
-	// preferences.putInt("mode", 0);
-	// preferences.end();
 }
 
 // Logic loop for taking user input and outputting the FFT
 void loop()
 {
+	Blynk.run();
 	// Check amount of free heap space initially
 	Serial.print("Free heap before running model: ");
 	Serial.println(String(ESP.getFreeHeap()));
@@ -266,7 +332,7 @@ void loop()
 
 		output_data.clear();
 		curr_sample++;
-		Serial.println("Inference count: " + String(curr_sample));
+		// Serial.println("Inference count: " + String(curr_sample));
 		delay(10);
 	}
 
@@ -315,4 +381,145 @@ void loop()
 	Serial.println("Going into deep sleep...");
 	esp_sleep_enable_timer_wakeup(deep_sleep_time);
 	esp_deep_sleep_start();
+}
+
+// Utility to extract header value from headers
+String getHeaderValue(String header, String headerName)
+{
+	return header.substring(strlen(headerName.c_str()));
+}
+
+// **************** OTA Utility Functions ****************
+// Function to get flash ESP32 with new firmware
+void execOTA()
+{
+	Serial.printf("Connecting to: %s\n", S3_HOST);
+	// Connect to S3
+	if (client.connect(S3_HOST, S3_PORT))
+	{
+		// Connection Succeed.
+		// Fecthing the bin
+		Serial.printf("Fetching Bin: %s\n", String(S3_BIN));
+
+		// Get the contents of the bin file
+		String request = String("GET ");
+		request += S3_BIN;
+		request += " HTTP/1.1\r\nHost: ";
+		request += S3_HOST;
+		request += "\r\nCache-Control: no-cache\r\nConnection: close\r\n\r\n";
+
+		client.print(request);
+
+		unsigned long timeout = millis();
+		while (client.available() == 0)
+		{
+			if (millis() - timeout > 5000)
+			{
+				Serial.println("Client Timeout !");
+				client.stop();
+				return;
+			}
+		}
+
+		while (client.available())
+		{
+			String line = client.readStringUntil('\n');
+			line.trim();
+
+			if (!line.length())
+			{
+				// headers ended
+				break; // and get the OTA started
+			}
+
+			if (line.startsWith("HTTP/1.1"))
+			{
+				if (line.indexOf("200") < 0)
+				{
+					Serial.println("Got a non 200 status code from server. Exiting OTA Update.");
+					break;
+				}
+			}
+
+			if (line.startsWith("Content-Length: "))
+			{
+				contentLength = atol((getHeaderValue(line, "Content-Length: ")).c_str());
+				Serial.printf("Got %s bytes from server\n", String(contentLength));
+			}
+
+			// Next, the content type
+			if (line.startsWith("Content-Type: "))
+			{
+				String contentType = getHeaderValue(line, "Content-Type: ");
+				Serial.printf("Got %s payload\n", contentType.c_str());
+				if (contentType == "application/octet-stream")
+				{
+					isValidContentType = true;
+				}
+			}
+		}
+	}
+	else
+	{
+		Serial.printf("Connection to %s failed. Please check your setup\n", String(S3_HOST));
+	}
+
+	// Check what is the contentLength and if content type is `application/octet-stream`
+	Serial.printf("contentLength : %s, isValidContentType : %s\n", String(contentLength), String(isValidContentType));
+
+	// check contentLength and content type
+	if (contentLength && isValidContentType)
+	{
+		// Check if there is enough to OTA Update
+		bool canBegin = Update.begin(contentLength);
+
+		// If yes, begin
+		if (canBegin)
+		{
+			Serial.println("OTA in progress...");
+			// No activity would appear on the Serial monitor
+			// So be patient. This may take 2 - 5mins to complete
+			size_t written = Update.writeStream(client);
+
+			if (written == contentLength)
+			{
+				Serial.printf("Written : %s successfully\n", String(written));
+				Blynk.virtualWrite(OTA_VPIN, 0); // Set OTA_VPIN to 0
+				Serial.printf("Setting OTA_VPIN to 0 to indicate OTA successful\n");
+			}
+			else
+			{
+				Serial.printf("Written only : %s/%s. Retry?\n", String(written), String(contentLength));
+			}
+
+			if (Update.end())
+			{
+				Serial.println("OTA done!");
+				if (Update.isFinished())
+				{
+					Serial.println("Update successfully completed. Rebooting.");
+					ESP.restart();
+				}
+				else
+				{
+					Serial.println("Update not finished? Something went wrong!");
+				}
+			}
+			else
+			{
+				Serial.printf("Error Occurred. Error #: %s\n", String(Update.getError()));
+			}
+		}
+		else
+		{
+			// not enough space to begin OTA
+			Serial.println("Not enough space to begin OTA");
+			client.flush();
+		}
+	}
+	else
+	{
+		Serial.println("There was no content in the response");
+		client.flush();
+	}
 }
